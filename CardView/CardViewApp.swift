4c6f8818faa1d5ad846e0abdeee13a957b3242af
Aleksandrfedotov <sasha4936@yandex.ru>
2023-10-05 12:43:03 +0500

    Initial Commit

---
+++ CardView/CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewApp.swift
+//  CardView
+//
+//  Created by Fedotov Aleksandr on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
